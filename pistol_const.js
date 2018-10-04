722745aad14838864f48d222f133d57a27094d17
pistol <biochemical27@gmail.com>
2018-10-03 19:25:43 -0700

    Aquarius website

---
+++ pistol_const.js
@@ -0,0 +1,71 @@
+var aries = prompt("Were you born between March 21st-April 20th?");
+
+if (aries==="yes") {
+	alert("Your sign is Aries, the ram ");
+}
+
+var taurus = prompt("Were you born between April 21st-May 20?");
+
+if (taurus==="yes") {
+	alert("Your sign is Taurus, the  bull.")
+}
+
+var gemini = prompt("Were you born between May 21st-Jun 21st?");
+
+if (gemini==="yes") {
+	 alert("Your sign is Gemini,the twins.")
+}
+
+var cancer = prompt("Were you born between Jun 22nd-Jul 22nd?");
+
+if (cancer==="yes") {
+	 alert("Your sign is Cancer, the crab.")
+}
+
+var leo = prompt("Were you born between Jul 23rd-Aug 23rd?");
+
+if (leo==="yes") {
+	 alert("Your sign is Leo, the lion.")
+}
+
+var virgo = prompt("Were you born between Aug 24th-Sept 23rd?");
+
+if (virgo==="yes") {
+	 alert("Your sign is virgo virgin.")
+}
+
+var libra = prompt("Were you born between Sept 24th-Oct 23rd?");
+
+if (libra==="yes") {
+	alert("Your sign is Libra, scales.")
+}
+
+var scorpio = prompt("Were you born between Oct 24th-Nov 22nd?");
+
+if (scorpio==="yes") {
+	 alert("Your sign is Scorpio, the Scorpon .")
+}
+
+var sagitt = prompt("Were you born between Nov 23rd-Dec 21st?");
+
+if (sagitt==="yes") {
+	 alert("Your sign is the Sagittarius, the centaur. ")
+}
+
+var capri = prompt("Were you born between Dec 22nd-Jan 20th?");
+
+if (capri==="yes") {
+	alert("Your sign is the Capricorn, the horned goat.")
+}
+
+var aqua = prompt("Were you born between Jan 21st-Feb 18th?");
+
+if (aqua==="yes") {
+	 alert("Your sign is Aquarius, the water bearer.")
+}
+
+var pisces= prompt("Were you born between Feb 19th-Mar 20th?");
+
+if (pisces==="yes") {
+	 alert("Your sign is Pisces, the fish.")
+}
